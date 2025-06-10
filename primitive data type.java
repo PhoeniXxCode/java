a924799875d4138859b2c5b357d538fb20db5d99
Shruti Baviskar <shrutibaviskar224@gmail.com>
2025-06-10 12:29:50 +0530

    Create primitive data type.java

---
+++ primitive data type.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int studentAge = 12;
+        double studentGpa = 9.0;
+        char studentFirstInitial = 's';
+        char studentLastInitial = 'i';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGpa);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
