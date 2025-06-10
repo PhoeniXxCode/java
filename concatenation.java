525ef805adb50e2f1c22140cf5f571f1d46ed419
Shruti Baviskar <shrutibaviskar224@gmail.com>
2025-06-10 19:11:45 +0530

    Create concatenation.java

---
+++ concatenation.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+       double studentGPA = 9.88;
+       String studentFirstName = "shruti";
+       String studentLastName = "Baviskar";
+       System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+    }
+}
